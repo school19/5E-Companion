b83eef6a425920edffa9d459f48e2ec731d82a0e
Charles Schooler <schooler.charles@gmail.com>
2017-02-28 23:02:46 -0500

    Fuck nosql.
    Loaders work with fragments and I didn't know that. NOSQL is no longer necessary and everything can still be done with XML.
    
    XML races will now include subrace tags with the id and file name of the subrace element. Classes will have a smiliar structure with how subclasses work.

--- app/src/main/java/com/schoolerc/ddcompanion/ui/display/CharacterDisplayActivity.java
+++ app/src/main/java/com/schoolerc/ddcompanion/ui/display/CharacterDisplayActivity.java
@@ -5,23 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.couchbase.lite.ManagerOptions;
-import com.couchbase.lite.android.AndroidContext;
 import com.schoolerc.ddcompanion.util.OnErrorListener;
 
 import com.schoolerc.ddcompanion.R;
 import com.schoolerc.ddcompanion.ui.creator.CharacterCreatorActivity;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
 
-import com.couchbase.lite.Manager;
 
-
-public class CharacterDisplayActivity extends Activity implements CharacterListFragment.OnEditCharacterListListener , OnErrorListener{
+public class CharacterDisplayActivity extends Activity implements CharacterListFragment.OnEditCharacterListListener, OnErrorListener{
 
     private static final int LOADER_ID_CHARACTERS = 0;
     private static final String KEY_CHARACTER_LIST = "key_character_list";
@@ -41,24 +33,6 @@ public class CharacterDisplayActivity extends Activity implements CharacterListF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character);
 
-        Manager manager = null;
-        deleteFile("test");
-        try{
-            FileOutputStream stream = openFileOutput("test", 0);
-            FileWriter writer = new FileWriter(stream.getFD());
-            writer.write("{}");
-            writer.close();
-            stream.close();
-            manager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
-            manager.getExistingDatabase("test");
-        }
-        catch(Exception ex)
-        {
-            Log.i(TAG, "fuuuuuuuuuuuuuuuuck");
-            System.exit(0);
-        }
-
-        Log.i(TAG, "Number of DBs: " + manager.getAllDatabaseNames().size());
         try {
             listFragment = (CharacterListFragment) getFragmentManager().findFragmentById(R.id.fragmentCharacterList);
             detailsFragment = (CharacterDetailsFragment) getFragmentManager().findFragmentById(R.id.fragmentCharacterDetails);
