8818b1fe6d9b397df9ea702f6e9ff316c620460c
Charles Schooler <schooler.charles@gmail.com>
2017-03-20 23:02:49 -0400

    Started the structure of how the composite\component loading is going to look

---
+++ app/src/main/java/com/schoolerc/ddcompanion/character/data/ComponentBuilder.java
@@ -0,0 +1,19 @@
+package com.schoolerc.ddcompanion.character.data;
+
+import com.schoolerc.ddcompanion.character.Component;
+
+/**
+ * Created by Chaz on 3/20/2017.
+ */
+
+public class ComponentBuilder {
+    private Component product;
+
+    public Component getComponent()
+    {
+        return product;
+    }
+
+    public void beginAbilityScore(){}
+    public void endAbilityScore(){}
+}
