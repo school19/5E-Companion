bf54e0b419f46cde77f402431136d50389bdabfd
Charles Schooler <schooler.charles@gmail.com>
2017-05-24 17:22:48 -0400

    Starting to write code to interact with the loader framework.

---
+++ Dungeon Companion/src/main/java/com/schoolerc/dungeoncompanion/loader/RaceLoader.java
@@ -0,0 +1,29 @@
+package com.schoolerc.dungeoncompanion.loader;
+
+import android.content.AsyncTaskLoader;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+
+import com.schoolerc.dungeoncompanion.data.Race;
+
+/**
+ * Created by Chaz Schooler on 5/24/2017.
+ */
+
+public class RaceLoader extends AsyncTaskLoader {
+
+    int id;
+
+    public RaceLoader(Context c, int id) {
+        super(c);
+        this.id = id;
+    }
+
+    @Override
+    public Object loadInBackground() {
+        Cursor cursor = getContext().getContentResolver().query(ContentUris.withAppendedId(Race.RaceContract.CONTENT_URI, id), null, null, null, null);
+        cursor.moveToNext();
+        return null;
+    }
+}
---
+++ Dungeon Companion/src/main/java/com/schoolerc/dungeoncompanion/ui/creator/RaceSelectorFragment.java
@@ -0,0 +1,96 @@
+package com.schoolerc.dungeoncompanion.ui.creator;
+
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.schoolerc.dungeoncompanion.data.Race;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chaz Schooler on 5/24/2017.
+ */
+
+public class RaceSelectorFragment extends Fragment{
+
+    private static final int ID_LOADER_ID = 1;
+    private static final int RACE_LOADER_ID_BASE = 2;
+
+    private static final String ARG_RACE_ID = "race_id";
+
+    private List<Race> raceData = new ArrayList<>();
+
+    private void clearRaces(){
+        raceData = new ArrayList<>();
+    }
+
+    private class RaceIdCallbacks implements LoaderManager.LoaderCallbacks<Cursor>{
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            return new CursorLoader(getContext(), Race.RaceContract.CONTENT_URI, new String[]{Race.RaceContract._ID}, null, null, null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            int idColumnIndex = data.getColumnIndex(Race.RaceContract._ID);
+            while(data.moveToNext())
+            {
+                int id = data.getInt(idColumnIndex);
+                Bundle args = new Bundle();
+                args.putInt(ARG_RACE_ID, id);
+
+                getLoaderManager().initLoader(RACE_LOADER_ID_BASE + id, args, new RaceLoaderCallbacks());
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            clearRaces();
+            //Also put logic here to indicate to the user the loader did not complete as expected
+        }
+    }
+
+    private class RaceLoaderCallbacks implements LoaderManager.LoaderCallbacks<Race>{
+        @Override
+        public Loader<Race> onCreateLoader(int id, Bundle args) {
+            return null;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Race> loader, Race data) {
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Race> loader) {
+
+        }
+    }
+
+    private void initiateDataLoad()
+    {
+        clearRaces();
+        getLoaderManager().initLoader(ID_LOADER_ID, null, new RaceIdCallbacks());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initiateDataLoad();
+    }
+}
