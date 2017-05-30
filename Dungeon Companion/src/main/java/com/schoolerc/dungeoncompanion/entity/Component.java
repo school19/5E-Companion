b4b504ccd497a1577188e14a77e7c0b7342e65ec
Charles Schooler <schooler.charles@gmail.com>
2017-05-30 07:58:25 -0400

    Added child hierarchy to the Component abstract class

--- Dungeon Companion/src/main/java/com/schoolerc/dungeoncompanion/entity/Component.java
+++ Dungeon Companion/src/main/java/com/schoolerc/dungeoncompanion/entity/Component.java
@@ -2,16 +2,36 @@ package com.schoolerc.dungeoncompanion.entity;
 
 import android.support.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by Chaz on 5/28/2017.
  */
 
-public abstract class Component implements Comparable<Component> {
+public abstract class Component implements Comparable<Component>, Iterable<Component>{
     public abstract void apply(CharacterState state);
     public abstract int getPriority();
+
+    public void addChild(Component c)
+    {
+        children.add(c);
+    }
+
+    public void removeChild(Component c)
+    {
+        children.remove(c);
+    }
+
+    private ArrayList<Component> children = new ArrayList<>();
 
     @Override
     public int compareTo(@NonNull Component o) {
         return Integer.valueOf(getPriority()).compareTo(o.getPriority());
+    }
+
+    @Override
+    public Iterator<Component> iterator() {
+        return children.iterator();
     }
 }
